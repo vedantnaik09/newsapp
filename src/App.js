cae8ce492e6bfc722ab2c55a7673bb79c876fe66
VedantPC <vedant.naik09@gmail.com>
2023-08-25 21:36:53 +0530

    Commit for CV

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import News from "./components/News";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Router>
+        <Navbar />        
+          <Routes>
+            <Route  path="/business"  element={<News key="business" pageSize={5} country="in" category="business" />}/>
+            <Route  path="/entertainment"element={<News key="entertainment"  pageSize={5} country="in" category="entertainment" />}/>
+            <Route  path="/" element={<News key="general" pageSize={5} country="in" category="general" />}/>
+            <Route  path="/health" element={<News key="health" pageSize={5} country="in" category="health" />}/>
+            <Route  path="/science" element={<News key="science" pageSize={5} country="in" category="science" />}/>
+            <Route  path="/sports" element={<News key="sports" pageSize={5} country="in" category="sports" />}/>
+            <Route  path="/technology" element={<News key="technology" pageSize={5} country="in" category="technology" />}/>
+          </Routes>
+          
+        </Router>
+      </div>
+    );
+  }
 }
-
-export default App;
