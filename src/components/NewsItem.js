7cf35909f36f39082fc0c5df4a02f00b6c14eaa4
VedantPC <vedant.naik09@gmail.com>
2023-08-25 22:34:56 +0530

    25-8

---
+++ src/components/NewsItem.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+
+export class NewsItem extends Component {
+  render() {
+    let { title, description, imageUrl, newsUrl, author, date ,source} = this.props;
+    return (
+      <div className="my-3">
+        
+        <div className="card" style={{ width: "auto", alignItems: "center" }}>
+        <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left:'90%',zIndex: '1'}}>
+                {source}
+              </span>
+          <img src={imageUrl} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">
+              {title}
+              
+            </h5>
+            <p className="card-text">{description}</p>
+            <p class="card-text">
+              <small class="text-muted">
+                By {author ? author : "Unknown"} on{" "}
+                {new Date(date).toGMTString()}
+              </small>
+            </p>
+            <a
+              rel="noreferrer"
+              href={newsUrl}
+              target="_blank "
+              className="btn btn-sm btn-dark"
+            >
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NewsItem;
